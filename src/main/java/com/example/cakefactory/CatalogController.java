887d0baa9f8d41bdd4842f5d7c0184e971b50f1a
kerwinlim25@yahoo.com <kerwinlim25@yahoo.com>
2022-05-01 00:50:52 -0400

    Add empty controller

--- src/main/java/com/example/cakefactory/CatalogController.java
+++ src/main/java/com/example/cakefactory/CatalogController.java
@@ -1,2 +1,15 @@
-package com.example.cakefactory;public class CatalogController {
+package com.example.cakefactory;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class CatalogController {
+
+    @GetMapping
+    String index(){
+        return "index";
+    }
 }
